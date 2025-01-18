cb1abb7388a49c5a0fec16766187a3cc793adafe
Loïc Reynier <loic@loicreynier.fr>
2025-01-18 20:32:22 +0100

    build(flake): update inputs

--- flake.nix
+++ flake.nix
@@ -4,62 +4,71 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = {
-    self,
-    flake-utils,
-    nixpkgs,
-    pre-commit-hooks,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      checks = {
-        pre-commit-check = pre-commit-hooks.lib.${system}.run {
-          src = ./.;
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      git-hooks,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        checks = {
+          pre-commit-check = git-hooks.lib.${system}.run {
+            src = ./.;
 
-          excludes = [
-            "flake\.lock"
-          ];
+            excludes = [
+              "flake\.lock"
+            ];
 
-          hooks = {
-            alejandra.enable = true;
-            commitizen.enable = true;
-            deadnix.enable = true;
-            editorconfig-checker.enable = true;
-            prettier = {
-              enable = true;
-              settings = {
-                ignore-unknown = true;
-                check = false;
-                write = true;
-                log-level = "silent";
+            hooks = {
+              commitizen.enable = true;
+              deadnix.enable = true;
+              editorconfig-checker.enable = true;
+              prettier = {
+                enable = true;
+                settings = {
+                  ignore-unknown = true;
+                  check = false;
+                  write = true;
+                  log-level = "silent";
+                };
               };
+              markdownlint.enable = true;
+              nixfmt-rfc-style.enable = true;
+              statix.enable = true;
+              typos.enable = true;
             };
-            markdownlint.enable = true;
-            statix.enable = true;
-            typos.enable = true;
           };
         };
-      };
 
-      devShells.default = pkgs.mkShell {
-        inherit (self.checks.${system}.pre-commit-check) shellHook;
-        propagatedBuildInputs = with pkgs; [
-          (python3.withPackages (ps:
-            with ps; [
-              mkdocs
-              mkdocs-material
-              mkdocs-git-revision-date-localized-plugin
-              pymdown-extensions
-            ]))
-          just
-        ];
-      };
-    });
+        devShells.default = pkgs.mkShell {
+          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          propagatedBuildInputs = with pkgs; [
+            (python3.withPackages (
+              ps: with ps; [
+                mkdocs
+                mkdocs-material
+                (mkdocs-git-revision-date-localized-plugin.overridePythonAttrs (old: {
+                  pyproject = true;
+                  format = null;
+                  dependencies = old.propagatedBuildInputs ++ [ setuptools-scm ];
+                }))
+              ]
+            ))
+            just
+          ];
+        };
+      }
+    );
 }
